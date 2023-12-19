1fe900a72352b0fe7c48f2c402dbcfaedbe6c58b
rohitsinha404 <sinharohit2003@gmail.com>
2023-12-19 23:33:55 +0530

    schema update

--- models/order.js
+++ models/order.js
@@ -76,7 +76,7 @@ const schema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.ObjectId,
     ref: "User",
-    required: true,
+    required: false,
   },
   totalPrice: {
     type: Number,
