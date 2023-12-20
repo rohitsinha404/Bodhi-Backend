e81af41fe80be7cbc3f953e7058f1a0f424e7c85
Rohit Sinha <114895566+rohitsinha404@users.noreply.github.com>
2023-12-20 20:07:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ config({
 // Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Using routes
 app.use("/api/v1/users", userRouter);
