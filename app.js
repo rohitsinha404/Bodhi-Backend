d44b2f3d30433658bc758e6490cbd5082ace874b
Rohit Sinha <114895566+rohitsinha404@users.noreply.github.com>
2023-12-20 19:58:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,11 +18,7 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
+  
 );
 
 // Using routes
